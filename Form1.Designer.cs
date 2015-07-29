e2726cf10edaaad9c5506cca09fd05a5c5b11f55
bruceburge <bruce.burge@gmail.com>
2015-07-29 08:34:44 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FileInfoOverlay
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tmrPaintCheck = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(70, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Loading...";
+			// 
+			// tmrPaintCheck
+			// 
+			this.tmrPaintCheck.Enabled = true;
+			this.tmrPaintCheck.Interval = 250;
+			this.tmrPaintCheck.Tick += new System.EventHandler(this.tmrPaintCheck_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(998, 33);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "File Info Overlay";
+			this.TopMost = true;
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer tmrPaintCheck;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileInfoOverlay
+{
+	public partial class Form1 : Form
+	{
+		//string windowTitle = Properties.Settings.Default.AppTitle;
+		string appName = Properties.Settings.Default.AppName;
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct RECT
+		{
+			public int X;
+			public int Y;
+			public int Width;
+			public int Height;
+		}
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+		public Form1()
+		{
+
+			InitializeComponent();
+		}
+
+		private void tmrPaintCheck_Tick(object sender, EventArgs e)
+		{
+			IntPtr hWnd = IntPtr.Zero; //FindWindow(null, "MainWindow");
+
+			var process = Process.GetProcessesByName(appName.Replace(".exe", "")).FirstOrDefault();
+			if (process != null)
+			{
+				hWnd = process.MainWindowHandle;
+				string fullPath = process.Modules[0].FileName;
+				//string ram = process.WorkingSet64.ToString();
+		
+				RECT rect;
+				if (hWnd.ToInt32() == 0)
+				{
+					label1.Text = "";
+				}
+				else
+				{
+					GetWindowRect(hWnd, out rect);
+					if (rect.X == -32000)
+					{
+						// the game is minimized
+						this.WindowState = FormWindowState.Minimized;
+						label1.Text = "";
+					}
+					else
+					{
+						this.WindowState = FormWindowState.Normal;
+						this.Location = new Point(rect.X + 10, rect.Y + 10);
+
+						FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(appName);
+						label1.Text = fullPath + " V. " + versionInfo.ProductVersion;
+					}
+				}
+			}
+		}
+	}
+}
