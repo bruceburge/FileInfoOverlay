57fcc411d58334d1c5ce3e234e313cd1009ae575
Bruce Burge <bruce@bruceburge.com>
2015-08-14 00:38:45 -0500

    Added buttons to kill and minimize watched process, also added ram usage.
    also added solution

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FileInfoOverlay
 {
-	partial class Form1
+	partial class FileInfoOverlay
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,50 +28,102 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.label1 = new System.Windows.Forms.Label();
-			this.tmrPaintCheck = new System.Windows.Forms.Timer(this.components);
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(0, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(70, 16);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Loading...";
-			// 
-			// tmrPaintCheck
-			// 
-			this.tmrPaintCheck.Enabled = true;
-			this.tmrPaintCheck.Interval = 250;
-			this.tmrPaintCheck.Tick += new System.EventHandler(this.tmrPaintCheck_Tick);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(998, 33);
-			this.Controls.Add(this.label1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "Form1";
-			this.ShowIcon = false;
-			this.Text = "File Info Overlay";
-			this.TopMost = true;
-			this.TransparencyKey = System.Drawing.SystemColors.Control;
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.tmrPaintCheck = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrPaintCheck
+            // 
+            this.tmrPaintCheck.Enabled = true;
+            this.tmrPaintCheck.Interval = 250;
+            this.tmrPaintCheck.Tick += new System.EventHandler(this.tmrPaintCheck_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnMinimize);
+            this.splitContainer1.Panel1.Controls.Add(this.btnKill);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(998, 29);
+            this.splitContainer1.SplitterDistance = 160;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(82, 3);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(75, 23);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "Minimize";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(3, 3);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(75, 23);
+            this.btnKill.TabIndex = 0;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading...";
+            // 
+            // FileInfoOverlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 29);
+            this.Controls.Add(this.splitContainer1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FileInfoOverlay";
+            this.ShowIcon = false;
+            this.Text = "File Info Overlay";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Timer tmrPaintCheck;
-	}
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMinimize;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,96 +14,127 @@ using System.Windows.Forms;
 
 namespace FileInfoOverlay
 {
-	public partial class Form1 : Form
-	{		
-		string _internalPath = string.Empty;
-		Point _offset = Properties.Settings.Default.DisplayOffSetInPixels;
-		
-		[StructLayout(LayoutKind.Sequential)]
-		public struct RECT
-		{
-			public int X;
-			public int Y;
-			public int Width;
-			public int Height;
-		}
+    public partial class FileInfoOverlay : Form
+    {
+        string _internalPath = string.Empty;
+        Process _watchedProcess = null;
+        Point _offset = Properties.Settings.Default.DisplayOffSetInPixels;
 
-		[DllImport("user32.dll", SetLastError = true)]
-		public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        const int WM_COMMAND = 0x111;
+        const int MIN_ALL = 419;
+        const int MIN_ALL_UNDO = 416;
 
-		[DllImport("user32.dll", SetLastError = true)]
-		public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int X;
+            public int Y;
+            public int Width;
+            public int Height;
+        }
 
-		public Form1()
-		{
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
 
-			InitializeComponent();
-		}
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
-		private void tmrPaintCheck_Tick(object sender, EventArgs e)
-		{
-			IntPtr hWnd = IntPtr.Zero; //FindWindow(null, "MainWindow");
+        public FileInfoOverlay()
+        {
+            Visible = false;
+            InitializeComponent();
+        }
 
-			var process = Process.GetProcessesByName(
-				Path.GetFileNameWithoutExtension(_internalPath)
-				).FirstOrDefault();
-			
-			if (process != null)
-			{
-				hWnd = process.MainWindowHandle;
-				//string fullPath = process.Modules[0].FileName;
-				//string ram = process.WorkingSet64.ToString();
-		
-				RECT rect;
-				if (hWnd.ToInt32() == 0)
-				{
-					label1.Text = "";
-				}
-				else
-				{
-					GetWindowRect(hWnd, out rect);
-					if (rect.X == -32000)
-					{
-						// the game is minimized
-						this.WindowState = FormWindowState.Minimized;
-						label1.Text = "";
-					}
-					else
-					{
-						this.WindowState = FormWindowState.Normal;
-						this.TopMost = true;
-						this.Location = new Point(rect.X + _offset.X, rect.Y + _offset.Y);
+        private void tmrPaintCheck_Tick(object sender, EventArgs e)
+        {
+            IntPtr hWnd = IntPtr.Zero; //FindWindow(null, "MainWindow");
 
-						FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(_internalPath);
-						label1.Text = _internalPath + " V. " + versionInfo.ProductVersion;
-					}
-				}
-			}
-		}
+             _watchedProcess = Process.GetProcessesByName(
+                Path.GetFileNameWithoutExtension(_internalPath)
+                ).FirstOrDefault();
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			_internalPath = Properties.Settings.Default.AppName;
-			 
-			//if the provided file doesn't exist, see if it exist in the current directory 
-			//this is to allow for full path, and local path names in config.
-			if (!File.Exists(_internalPath))
-			{
-				string combinePath = Path.Combine(Environment.CurrentDirectory, _internalPath);
-				if (File.Exists(combinePath))
-				{
-					_internalPath = combinePath;
-				}
-				else
-				{
-					MessageBox.Show("File not found : " + _internalPath);
-					Application.Exit();
-				}
-			}
+            if (_watchedProcess != null)
+            {
+                hWnd = _watchedProcess.MainWindowHandle;
+                //string fullPath = process.Modules[0].FileName;
+                //string ram = process.WorkingSet64.ToString();
 
-			label1.ForeColor = Properties.Settings.Default.ForeGround;
-			label1.BackColor = Properties.Settings.Default.BackGround;
+                RECT rect;
+                if (hWnd.ToInt32() == 0)
+                {
+                    label1.Text = "";
+                    Visible = false;
+                }
+                else
+                {
+                    Visible = true;
+                    GetWindowRect(hWnd, out rect);
+                    if (rect.X == -32000)
+                    {
+                        // the game is minimized
+                        this.WindowState = FormWindowState.Minimized;
+                        label1.Text = "";
+                    }
+                    else
+                    {
+                        this.WindowState = FormWindowState.Normal;
+                        this.TopMost = true;
+                        this.Location = new Point(rect.X + _offset.X, rect.Y + _offset.Y);
 
-		}
-	}
+                        FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(_internalPath);
+                        label1.Text = _internalPath + " V. " + versionInfo.ProductVersion+" ram: "+ _watchedProcess.WorkingSet64/ 1048576 + " mb";
+                    }
+                }
+            }
+            else
+            {
+                Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Visible = false;
+            _internalPath = Properties.Settings.Default.AppName;
+
+            //if the provided file doesn't exist, see if it exist in the current directory 
+            //this is to allow for full path, and local path names in config.
+            if (!File.Exists(_internalPath))
+            {
+                string combinePath = Path.Combine(Environment.CurrentDirectory, _internalPath);
+                if (File.Exists(combinePath))
+                {
+                    _internalPath = combinePath;
+                }
+                else
+                {
+                    MessageBox.Show("File not found : " + _internalPath);
+                    Application.Exit();
+                }
+            }
+
+            label1.ForeColor = Properties.Settings.Default.ForeGround;
+            label1.BackColor = Properties.Settings.Default.BackGround;
+
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            if(_watchedProcess != null)
+            {
+                Visible = false;
+                _watchedProcess.Kill();
+                tmrPaintCheck.Stop();
+                Environment.Exit(0);
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+            SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL, IntPtr.Zero);
+        }
+    }
 }
